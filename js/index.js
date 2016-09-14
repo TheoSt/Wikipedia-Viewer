d48e6bf605601713bb5447df88461ae812b3f4ef
Theodore Stamatiadis <stamatiadisteo@gmail.com>
2016-09-14 20:36:44 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+});
+
+ function search(ev) {
+    ev.preventDefault();
+    var input = $("#in").val();
+    $("#info").removeAttr('class');
+    $.ajax ({
+      type:"GET",
+      url:"http://en.wikipedia.org/w/api.php?action=opensearch&search="+input+"&limit=15&namespace=0&format=json&callback=?",
+      contentType:"application/json; charset=utf-8",
+      async:false,
+      dataType:"json",
+      success: function(data) {
+        if($(".results").length) {
+          $(".results").remove();
+        }
+        for(var i=0; i<data[1].length; i++) {
+           $(".wiki_results").append("<ul class='results'><li><a class='wiki_links' href='"+data[3][i]+"' target='_blank'><p><h2>"+data[1][i]+"</h2>"+data[2][i]+"</p></a></li></ul>");
+        }
+      }
+    });
+ }
